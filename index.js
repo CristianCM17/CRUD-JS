2c02a6d87f9c230f7936df65ef4570aa9fd6ba19
CristianCM17 <cristiandavidcardoso@gmail.com>
2023-05-02 12:32:22 -0600

    Crea y elimina la fila

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ document.addEventListener('DOMContentLoaded',function () {
     const alert= document.getElementById('alert');
     const table= document.getElementById('table');
     const btn= document.getElementById('add');  
+    let id=1; //el id de cada fila
+    
+    function removeTOdo(id) { //funcion para remover la fila, recibe el id de la fila
+        
+        document.getElementById(id).remove();
+       
+
+        
+    }
+    
+    
+    
 
     function addTodo() {
         if (title.value==='' && description.value==='') {
@@ -12,12 +24,36 @@ document.addEventListener('DOMContentLoaded',function () {
             alert.innerText= 'Title and description are requiered'; //cambiamos lo que dice la alerta
             return;
         }
+         
         alert.classList.add('d-none');//anadimos el display-none
-    }
 
-        btn.onclick= addTodo;
-
-
-
-
+        const row= table.insertRow(); //insertamos una nueva fila
+        //agregamos al html el contenido 
+        row.setAttribute('id',id++) //le damos un atributo a cada fila que es un id que estara aumentando
+        row.innerHTML= `  
+        <td>${title.value}</td>
+        <td>${description.value}</td>
+        <td class="text-center">
+                <input type="checkbox">
+              </td>
+              <td class="text-right">
+                <button class="btn btn-primary mb-1">
+                  <i class="fa fa-pencil"></i>
+                </button>
+        </td>
+        `;
+        const removeBtn= document.createElement('button'); //creamos un boton
+        removeBtn.classList.add('btn','btn-danger', 'mb-1');//le damos el css
+        removeBtn.innerHTML=  '<i class="fa fa-trash"><i>'; //le damos el logo del boton
+        removeBtn.onclick= function () { //con el click llamamos a la funcion removeTodo y le 
+          removeTOdo(row.getAttribute('id')) //recuperamos el id de la fila en la que estamos
+            
+        }
+        row.children[3].appendChild(removeBtn); //el espacio 4 donde ponemos los botones
+                                                //le agregaremos elboton removebtn
+        }
+        
+    
+    btn.onclick= addTodo; //llamar a lafuncion asi porque si no a fuerzas quiere algo de retorno
 });
+z

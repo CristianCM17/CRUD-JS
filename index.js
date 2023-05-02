f4669613fb5dd5ca2d6f1606fbcb102501c95f98
CristianCM17 <cristiandavidcardoso@gmail.com>
2023-05-02 12:07:41 -0600

    Valide la entrada nula de los input

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+//el evento del content loaded que no noe ejecute nada de js hasta que todo el html se haya cargado
+document.addEventListener('DOMContentLoaded',function () {
+    const title= document.getElementById('title');
+    const description= document.getElementById('description');
+    const alert= document.getElementById('alert');
+    const table= document.getElementById('table');
+    const btn= document.getElementById('add');  
 
+    function addTodo() {
+        if (title.value==='' && description.value==='') {
+            alert.classList.remove('d-none');//la alerta se le remueve la clase de css de display-none
+            alert.innerText= 'Title and description are requiered'; //cambiamos lo que dice la alerta
+            return;
+        }
+        alert.classList.add('d-none');//anadimos el display-none
+    }
+
+        btn.onclick= addTodo;
+
+
+
+
+});

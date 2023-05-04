14fe771082b6351dcf4b5d9119745562f3bad0a5
CristianCM17 <cristiandavidcardoso@gmail.com>
2023-05-03 22:31:48 -0600

    Terminacion

--- js/components/add-todo.js
+++ js/components/add-todo.js
@@ -1,20 +1,22 @@
+import Alert from './alert.js';
 
-export default class AddTodo{
-    constructor(){
+export default class AddTodo {
+  constructor() {
     this.btn = document.getElementById('add');
     this.title = document.getElementById('title');
     this.description = document.getElementById('description');
-    }
 
-    onClick(callback){ //le estamos pasando una funcion
-        this.btn.onclick = () => {
-            if (title.value === '' || description.value === '') {
-              
-                console.error('incorrecto');
-            }else{
-                callback(this.title.value,this.description.value);//si todo esta correcto le pasamos a la funcion que nos dieron esos argumentos
-            }
-        }
-    }
+    this.alert = new Alert('alert');
+  }
 
+  onClick(callback) {
+    this.btn.onclick = () => {
+      if (title.value === '' || description.value === '') {
+        this.alert.show('Title and description are required');
+      } else {
+        this.alert.hide();
+        callback(this.title.value, this.description.value);
+      }
+    }
+  }
 }
--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 import Model from './model.js';
-import View  from './view.js';
+import View from './view.js';
 
-document.addEventListener('DOMContentLoaded', function () {
-    const model = new Model(); //instanciamos la clase model
-    const view = new View(); //instanciamos la clase view
-    model.setView(view); //le pasamos el view al model da igual que sea
-    view.setModel(model);//le pasamos el model al view da igual que sea
+document.addEventListener('DOMContentLoaded', () => {
+  const model = new Model();
+  const view = new View();
+  model.setView(view);
+  view.setModel(model);
+
+  view.render();
 });
